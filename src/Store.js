60fc8a0046e462bf8512d4a2f3731bc218d3fd91
Miles Grimes <mgrimes6@oreillyauto.com>
2017-10-28 11:26:52 -0500

    Implemented Fleet Beacon upgrade

--- src/Store.js
+++ src/Store.js
@@ -147,6 +147,14 @@ const helpers = {
         total *= 2
       }
 
+      if (upgradePurchased('Fleet Beacon')) {
+        if (purchased < 100) {
+          total *= 2
+        } else {
+          total *= 3
+        }
+      }
+
       return Math.floor(total)
     }
   },
@@ -269,6 +277,9 @@ const upgrades = {
   ]),
   'Aria Hammera': upgrade('Aria Hammera', '+1 Hammer power per 15 Hammers', 600000, [
     preReq(Constants.PREREQ.HELPER.NUMBER, 'Hammer', 15)
+  ]),
+  'Fleet Beacon': upgrade('Fleet Beacon', '+100% Airplane power. +200% at 100 Airplanes', 1750000, [
+    preReq(Constants.PREREQ.HELPER.NUMBER, 'Airplane', 25)
   ]),
   'The Awakening': upgrade('The Awakening', 'Djinn sacrifice current power to reach full potential', 5000000, [
     preReq(Constants.PREREQ.HELPER.NUMBER, 'Djinn', 10)
