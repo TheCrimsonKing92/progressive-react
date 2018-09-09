61096da8d58e6a361e095c53650c6c6f873482ad
TheCrimsonKing92 <mgrimes@millikin.edu>
2018-09-09 10:32:00 -0500

    Use of Constants and improvements in Buyable

--- src/Buyable.js
+++ src/Buyable.js
@@ -2,10 +2,15 @@ import React, {PureComponent} from 'react'
 import {findDOMNode} from 'react-dom'
 import ReactTooltip from 'react-tooltip'
 
+import Constants from './Constants'
+
 const getClassName = name => name.replace(' ', '-')
                                  .replace(/\./g, '-')
                                  .toLowerCase()
-const isFadeable = type => (type === 'tower' || type === 'upgrade')
+const isFadeable = type => (type === Constants.BUYABLE_TYPE.TOWER || type === Constants.BUYABLE_TYPE.UPGRADE)
+const noSelect = {
+  userSelect: 'none'
+}
 class Buyable extends PureComponent {
   constructor(props) {
     super(props)
@@ -24,8 +29,8 @@ class Buyable extends PureComponent {
     const fade = isFadeable(this.props.buyable.type) && this.props.fade && !this.props.buyable.buyable ? ' faded' : ''
 
     return (
-      <div data-tip data-for={`buyable${className}`} key={this.props.buyable.id} className={`buyable ${className}${fade}`} style={{ 'userSelect': "none"}} onClick={() => this.handlePurchase(this.props.buyable)}>
-        <ReactTooltip ref='tooltip' border={true} getContent={[() => this.props.buyable.tooltip, 150]} id={`buyable${className}`}  html={true}/>
+      <div data-tip data-for={`buyable${className}`} key={this.props.buyable.id} className={`buyable ${className}${fade}`} style={noSelect} onClick={() => this.handlePurchase(this.props.buyable)}>
+        <ReactTooltip ref='tooltip' border={true} getContent={[() => this.props.buyable.tooltip, 100]} id={`buyable${className}`}  html={true}/>
       </div>
     )
   }
