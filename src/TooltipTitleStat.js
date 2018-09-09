0105fe1653abeacbdabdac26a073b41a578609f1
TheCrimsonKing92 <mgrimes@millikin.edu>
2018-09-09 16:21:36 -0500

    TooltipTitleStat, Component -> PureComponent

--- src/TooltipTitleStat.js
+++ src/TooltipTitleStat.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react'
+import React, {PureComponent} from 'react'
 import ReactTooltip from 'react-tooltip'
 
-class TooltipTitleStat extends Component {
+class TooltipTitleStat extends PureComponent {
   render() {
     return (
       <div className="TitleStat">
